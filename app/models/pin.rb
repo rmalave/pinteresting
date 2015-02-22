09ccf3b871d85cd8262427e115849917c9753630
Rafael Malave <rafomalave@gmail.com>
2015-02-21 17:09:23 -0800

    fix some files

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,8 @@
 class Pin < ActiveRecord::Base
-    belongs_to :user
-    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }    
+	belongs_to :user
 
-    validates :description, presence: true
-    validates :image, presence: true
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+	validates :image, presence: true
+	validates :description, presence: true
 end
